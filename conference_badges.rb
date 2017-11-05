b9a5340249d9cab2b3e13c3efd287a77c124b4a4
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-05 21:33:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,7 @@ end
 def batch_badge_creator(names)
   names.collect {|name| badge_maker(name)}
 end
+
+def assign_rooms(names)
+  names.collect {|name| "Hello, #{name}! You'll be assigned to room #{names.index(name)}!"}
+end
